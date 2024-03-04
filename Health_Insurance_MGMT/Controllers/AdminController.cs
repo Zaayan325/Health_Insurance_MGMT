677ac013035c8a5af3955961ab568527c8581c0d
Zaayan325 <150224587+Zaayan325@users.noreply.github.com>
2024-03-04 18:17:58 +0500

    Admin Controller Updated

--- Health_Insurance_MGMT/Controllers/AdminController.cs
+++ Health_Insurance_MGMT/Controllers/AdminController.cs
@@ -37,5 +37,40 @@ namespace Health_Insurance_MGMT.Controllers
             }
             return View();
         }
+
+        public IActionResult ViewEmp()
+        {
+            //Select * from table EmpRegister
+            IEnumerable<EmpRegister> empRegisters = _unitofWork.EmpRegisterRepository.GetAll();
+            return View(empRegisters);
+        }
+        // This Method will only return view with selected EmpRegister
+        public IActionResult Delete(int? empno)
+        {
+            if (empno == null || empno == 0)
+            {
+                return NotFound();
+            }
+            var empreg = _unitofWork.EmpRegisterRepository.GetT(er => er.empno == empno);
+            if (empreg == null)
+            {
+                return NotFound();
+            }
+            return View(empreg);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult DeleteData(int? empno) 
+        {
+            var empreg = _unitofWork.EmpRegisterRepository.GetT(er => er.empno == empno);
+            if (empreg == null) 
+            {
+                return NotFound();
+            }
+            _unitofWork.EmpRegisterRepository.Delete(empreg);
+            _unitofWork.save();
+            return RedirectToAction();
+        }
     }
 }
